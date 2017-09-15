9b7faa94fda0e89ea9737f264708c4170446e35d
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-09-16 02:53:51 +0900

    主人公のHP表示した

--- Shoooting/Player.cpp
+++ Shoooting/Player.cpp
@@ -2,6 +2,7 @@
 #include "Player.hpp"
 #include "define.hpp"
 #include "UseDxLib.hpp"
+#include <string>
 
 namespace game {
 	Player::Player()
@@ -10,6 +11,11 @@ namespace game {
 	}
 	void Player::Update()
 	{
+		//プレイヤーHP表示     
+		//std::to_stringで整数をstringに変換
+		std::string str =("HP:" + std::to_string(m_hp));
+		DxLib::DrawString(10, 400, str.c_str(), DxLib::GetColor(255, 255, 255));
+
 		//プレイヤー表示
 		DxLib::DrawString(GetPos().x, GetPos().y, "自", DxLib::GetColor(255, 255, 255));
 	}
--- Shoooting/Player.hpp
+++ Shoooting/Player.hpp
@@ -6,6 +6,8 @@ namespace game {
 	//Ž©‹@
 	class Player:public GameObject {
 
+		//‘Ì—Í
+		int m_hp = 5;
 	public:
 		Player();
 		virtual void Update()final override;
